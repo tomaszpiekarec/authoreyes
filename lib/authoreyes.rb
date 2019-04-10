21ca2c7a224dc7eff4a7c2335c8626c2096ff8d2
tomaszpiekarec <ziraelqq@gmail.com>
2019-04-10 10:16:02 +0200

    Update authoreyes.rb

--- lib/authoreyes.rb
+++ lib/authoreyes.rb
@@ -3,13 +3,13 @@ require 'authoreyes/version'
 require 'authoreyes/parser'
 require 'authoreyes/authorization'
 require 'authoreyes/railtie'
+require 'authoreyes/helpers'
 
 module Authoreyes
   class Railtie
     # Require helpers after Rails initialization
     config.after_initialize do
-      require 'authoreyes/helpers'
-
+      
       # Include Controller helpers
       ActionController::Metal.include Authoreyes::Helpers::InController
 
